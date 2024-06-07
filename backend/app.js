784d0e2822b192c00fe3f18da33223c1aa44aff9
aryanmamania <aryanmamania17@gmail.com>
2024-06-07 12:27:28 +0530

    started working on backend

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db')
+const {readdirSync} = require ('fs')
 
 
-const app = express()
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT 
 
-
-//middlewares
-app.use(express.json())
-app.use(cors())
-
+// Middlewares
+app.use(express.json());
+app.use(cors());
 
 
-const server = () =>{
-app.listen(PORT, ()=>{
-    console.log('listening to port:', PORT)
-})
-}
+//routes
+readdirSync('./routes').map((route)=> app.use('/api/v1', require('./routes/' + route )))
 
-server()
+const server = () => {
+ db(); 
+    app.listen(PORT, () => {
+        console.log('Listening on port:', PORT);
+    });
+};
+
+server();
