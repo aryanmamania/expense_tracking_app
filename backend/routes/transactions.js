3d7ffe61cbcb02b6da30d01978f535acdd74e806
aryanmamania <aryanmamania17@gmail.com>
2024-06-09 20:10:35 +0530

    working on backend

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,8 +1,15 @@
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense')
+const { addincome, deleteIncomes, getIncomes} = require('../controllers/income')
+
 const router = require('express').Router()
 
 
-router.get('/', (req, res)=>{
-    res.send('Hello World')
-})
+router.post('/add-income', addincome)
+router.get('/get-income', getIncomes)
+router.delete('/delete-income/:id' , deleteIncomes)
+
+router.post('/add-expense', addExpense)
+router.get('/get-expense', getExpense)
+router.delete('/delete-expense/:id' , deleteExpense)
 
 module.exports = router 
